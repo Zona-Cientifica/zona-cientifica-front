ef0fdeab6d5c363c9e9ded33d5142c2aee1fb381
Robson William <robsonwilliamro@gmail.com>
2023-12-11 21:28:20 -0300

    Criando filtro de posts

--- src/screens/AllEventsScreen/AllEventsScreen.tsx
+++ src/screens/AllEventsScreen/AllEventsScreen.tsx
@@ -1,50 +1,18 @@
-import { View, Text, ImageBackground, FlatList } from "react-native";
+import { View, Text, ImageBackground, FlatList, Pressable } from "react-native";
 import styles from "./style";
 import Category from "../../components/Category/Category";
 import { Card } from "../../components/Card/card";
 import { api } from "../../utils/api";
-import { useState } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 type Events = {
   title: string;
   picture: string;
   description: string;
   date: string;
+  theme: string
 };
 
-/*const events = [
-  {
-    title: "IV SertãoComp",
-    picture:
-    "https://s3-alpha-sig.figma.com/img/e25e/f73a/1287e212956d8313fb8b12163d14f89b?Expires=1700438400&Signature=NkXNU8CcHrw0HX0TCWO1kAcHc5YVNXiZyO1YmIQDbOSxVqq01YBU71IJeVL0rxoNDb8SdB5Iqfgcl6q8LJmoyW1MVGph5fF6ELqkhocDOTx-1Vh-v~8xbszFPzo855qr4NAm8-PIS0SBdyztSaQ1z31KzRnz9GDNcWzleWbKm7eQ3jLYpABcCJGtlDs7gYYXPU4a6Te1MUboH4fiI-uE9PZALsBspqG5X2j-7-JqLbGBVCLAoyYRLa2sWJz7ktU4jT-Db2Mbbx-R6m6tdOGwOBXnZxln0-uInAXzN9dS85a8o4i5NPRSWxU-OFmxI2kZMSu7QS8YfseL-rWFWcn9ig__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-    description:
-    "4° edição do SertãoComp, acontecerá presencialmente no campus Cajazeiras.",
-    date: "04 a 07 de Outubro",
-  },
-  {
-    title: "43º CSBC",
-    picture:
-    "https://s3-alpha-sig.figma.com/img/9239/7976/2f150cbdcf8a4f10f35ccce7b4035210?Expires=1700438400&Signature=aaAT~5I2X9XaiXeco1MSOQrTDh8c6M-hu5Sl6oFmNbP6D2FN-vkoFofAGCBx968AwNfr0saA0bRsyIwQJRfYYiVjPS6TrqpgzJ9h7guuv8ZYwmyrlye7YHRi5HF0kCxoy91f8rscRvormZw6Volo~d5cWyfl65S7--WcSrqY9oMPuHmjrq84CMthpmMbXxHHWt~3Rq0qCTd5Vi~sq1sMraKdwGfnxH-WoShzyDZ1waYa-hSn~3wplQBukoffwQrmBND~RNk3WB3ukPSInLOF3BauQjxRDSC-WIIRYFE6biqbm66bdVhJ2nu5fXSm8SJsSrnObPczBcGL1je4OrEunA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-    description: "O Congresso da Sociedade Brasileira de Computação",
-    date: "06 a 11 de Agosto",
-  },
-  {
-    title: "IV SertãoComp",
-    picture:
-    "https://s3-alpha-sig.figma.com/img/e25e/f73a/1287e212956d8313fb8b12163d14f89b?Expires=1700438400&Signature=NkXNU8CcHrw0HX0TCWO1kAcHc5YVNXiZyO1YmIQDbOSxVqq01YBU71IJeVL0rxoNDb8SdB5Iqfgcl6q8LJmoyW1MVGph5fF6ELqkhocDOTx-1Vh-v~8xbszFPzo855qr4NAm8-PIS0SBdyztSaQ1z31KzRnz9GDNcWzleWbKm7eQ3jLYpABcCJGtlDs7gYYXPU4a6Te1MUboH4fiI-uE9PZALsBspqG5X2j-7-JqLbGBVCLAoyYRLa2sWJz7ktU4jT-Db2Mbbx-R6m6tdOGwOBXnZxln0-uInAXzN9dS85a8o4i5NPRSWxU-OFmxI2kZMSu7QS8YfseL-rWFWcn9ig__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-    description:
-    "4° edição do SertãoComp, acontecerá presencialmente no campus Cajazeiras.",
-    date: "04 a 07 de Outubro",
-  },
-  {
-    title: "43º CSBC",
-    picture:
-    "https://s3-alpha-sig.figma.com/img/9239/7976/2f150cbdcf8a4f10f35ccce7b4035210?Expires=1700438400&Signature=aaAT~5I2X9XaiXeco1MSOQrTDh8c6M-hu5Sl6oFmNbP6D2FN-vkoFofAGCBx968AwNfr0saA0bRsyIwQJRfYYiVjPS6TrqpgzJ9h7guuv8ZYwmyrlye7YHRi5HF0kCxoy91f8rscRvormZw6Volo~d5cWyfl65S7--WcSrqY9oMPuHmjrq84CMthpmMbXxHHWt~3Rq0qCTd5Vi~sq1sMraKdwGfnxH-WoShzyDZ1waYa-hSn~3wplQBukoffwQrmBND~RNk3WB3ukPSInLOF3BauQjxRDSC-WIIRYFE6biqbm66bdVhJ2nu5fXSm8SJsSrnObPczBcGL1je4OrEunA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-    description: "O Congresso da Sociedade Brasileira de Computação",
-    date: "06 a 11 de Agosto",
-  },
-];
-*/
 const category = [
   {
     name: "Tecnologia",
@@ -71,6 +39,7 @@ const category = [
 export default function AllEventsScreen() {
   
   const [events, setEvents] = useState<Events[]>([]);
+  const [filter, setFilter] = useState("NONE");
 
   async function findEvents() {
     try {
@@ -81,7 +50,26 @@ export default function AllEventsScreen() {
       console.log("ERRO: " + error);
     }
   }
-  findEvents();
+
+  // async function findCategorys() {
+    
+  // }
+  
+  useEffect(() => {
+    findEvents();
+    // findCategorys();
+  }, [])
+
+  const filteredContent = useMemo(() => {
+    if(filter === "NONE"){
+      return events;
+    }
+    return events.filter((item) => item.theme === filter)
+  }, [filter, events])
+
+  const handleFilterChange = (filter:string) => {
+    setFilter(filter)
+  }
 
   return (
     <>
@@ -95,9 +83,13 @@ export default function AllEventsScreen() {
 
             <FlatList
               data={category}
-              renderItem={({ item }) => <Category category={item} />}
+              renderItem={({ item }) => <Category category={item} handleFilter={handleFilterChange}/>}
               horizontal
             />
+
+            <Pressable onPress={() => handleFilterChange("NONE")}>
+              <Text>limpar</Text>
+            </Pressable>
           </View>
 
           <View style={styles.boxEvents}>
@@ -105,7 +97,7 @@ export default function AllEventsScreen() {
 
             <View style={styles.events}>
               <FlatList
-                data={events}
+                data={filteredContent}
                 renderItem={({ item }) => <Card event={item} />}
               />
             </View>
