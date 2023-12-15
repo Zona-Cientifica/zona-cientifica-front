c232ec1faf9f9ce51604d525775a3af210eb0c5c
Robson William <robsonwilliamro@gmail.com>
2023-12-15 13:56:06 -0300

    Adicionando cadastro de eventos

---
+++ src/components/InputMapPosition/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      margin: 0
+    },
+    sendLogin: {
+      marginTop: "30%",
+      alignSelf: "center",
+      backgroundColor: "blue"
+    }
+})
+
+export default styles;
