34bbeaab0b7235024d41bb7e1e80c4e438270d19
leomendes18 <leomendesmonteiro1@gmail.com>
2023-12-15 22:37:32 -0300

    arrumando visualização do mapa

--- src/components/InputMapPosition/InputMapPosition.tsx
+++ src/components/InputMapPosition/InputMapPosition.tsx
@@ -1,10 +1,5 @@
 import { useEffect, useState } from "react";
-import {
-    Alert,
-    Pressable,
-    View,
-    Text
-} from "react-native";
+import { Alert, Pressable, View, Text } from "react-native";
 import * as Location from "expo-location";
 import Map from "../Map/Map";
 import styles from "./styles";
@@ -12,81 +7,92 @@ import { Marker, MapPressEvent } from "react-native-maps";
 import { useAuth } from "../../contexts/auth";
 
 type Props = {
-    navigation: any
-}
+  navigation: any;
+};
 
 type Position = {
-    latitude:number,
-    longitude:number
-}
+  latitude: number;
+  longitude: number;
+};
 
-export default function InputMapPosition({navigation}:Props) {
-    const {signed} = useAuth();
+export default function InputMapPosition({ navigation }: Props) {
+  const { signed } = useAuth();
 
-    const [currentPosition, setCurrentPosition] = useState<Position>({latitude: 0, longitude: 0});
-    const [chosedPosition, setChosedPosition] = useState<Position>({latitude: 0, longitude: 0});
+  const [currentPosition, setCurrentPosition] = useState<Position | null>(null);
+  const [chosedPosition, setChosedPosition] = useState<Position>({
+    latitude: 0,
+    longitude: 0,
+  });
 
-    function handleSelectPosition(event:MapPressEvent){
-        setChosedPosition(event.nativeEvent.coordinate);
-        console.log(chosedPosition)
+  function handleSelectPosition(event: MapPressEvent) {
+    setChosedPosition(event.nativeEvent.coordinate);
+    console.log(chosedPosition);
+  }
+
+  async function getCurrentLocation() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert("Precisamos da sua localização!");
+      return;
     }
 
-    async function getCurrentLocation(){
-        let {status} = await Location.requestForegroundPermissionsAsync();
+    let location = await Location.getCurrentPositionAsync();
+    setCurrentPosition(location.coords);
+  }
 
-        if(status !== "granted"){
-            Alert.alert("Precisamos da sua localização!");
-            return
-        }
+  function sendNextPage() {
+    navigation.navigate("CreateEventPost", { position: chosedPosition });
+  }
 
-        let location = await Location.getCurrentPositionAsync();
-        setCurrentPosition(location.coords);
-    }
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
 
-    function sendNextPage(){
-        navigation.navigate("CreateEventPost", {position: chosedPosition}); 
-    }
+  function handleSendPage() {
+    navigation.navigate("Login");
+  }
 
-    useEffect(() => {
-        getCurrentLocation()
-    }, [])
-    
-    function handleSendPage(){
-        navigation.navigate("Login")
-    }
-
-    return (
-        <>
-            {signed ?
-            <View style={styles.container}>
-                <Map
-                    initialRegion={{
-                        latitude: currentPosition.latitude,
-                        longitude: currentPosition.longitude,
-                        latitudeDelta: 0.008,
-                        longitudeDelta: 0.008
+  return (
+    <>
+      {signed ? (
+        <View style={styles.container}>
+          {currentPosition ? (
+            <Map
+              initialRegion={{
+                latitude: currentPosition.latitude,
+                longitude: currentPosition.longitude,
+                latitudeDelta: 0.008,
+                longitudeDelta: 0.008,
+              }}
+              showUserLocation={true}
+              onPress={handleSelectPosition}
+            >
+              {chosedPosition.latitude !== 0 &&
+                chosedPosition.longitude !== 0 && (
+                  <Marker
+                    coordinate={{
+                      latitude: chosedPosition.latitude,
+                      longitude: chosedPosition.longitude,
                     }}
-                    showUserLocation
-                    onPress={handleSelectPosition}
-                >
-                    {chosedPosition.latitude !== 0 && chosedPosition.longitude !== 0 && (
-                        <Marker
-                            coordinate ={{
-                            latitude: chosedPosition.latitude,
-                            longitude: chosedPosition.longitude,
-                            }}
-                        />
-                    )}
-                </Map>
-
-                {chosedPosition.latitude !== 0 && chosedPosition.longitude && (
-                    <Pressable onPress={sendNextPage}>
-                        <Text>Próxima página</Text>
-                    </Pressable>
+                  />
                 )}
-            </View>
-            : 
-            <Pressable onPress={handleSendPage} style={styles.sendLogin}><Text>Clica aqui para fazer login</Text></Pressable>}
-        </>
-    );
+            </Map>
+          ) : (
+            <Text>mapa sumiu</Text>
+          )}
+
+          {chosedPosition.latitude !== 0 && chosedPosition.longitude && (
+            <Pressable onPress={sendNextPage}>
+              <Text>Próxima página</Text>
+            </Pressable>
+          )}
+        </View>
+      ) : (
+        <Pressable onPress={handleSendPage} style={styles.sendLogin}>
+          <Text>Clica aqui para fazer login</Text>
+        </Pressable>
+      )}
+    </>
+  );
 }
